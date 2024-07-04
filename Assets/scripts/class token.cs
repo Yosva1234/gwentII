4a1c2b666a66a496450631898a78a8ece7c0684b
Yosva1234 <158980095+Yosva1234@users.noreply.github.com>
2024-07-04 09:47:51 -0400

    interfaz Itoken e implementacion de la clase token

---
+++ Assets/scripts/class token.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gwentii{
+
+public interface Itoken
+{
+    int  fila {get; set;}
+    int columna {get; set;}
+
+
+
+}
+
+public class token // Itoken
+{
+
+    
+
+}
+
+
+
+
+}
---
+++ Assets/scripts/codigo matriz.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+public class codigomatriz : MonoBehaviour
+{
+   
+    public TMP_InputField codigo;
+  
+}
